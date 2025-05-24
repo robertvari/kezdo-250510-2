847e073b0d48d64b1d850f3d184c47b015195102
Robert Vari <mail.robertvari@gmail.com>
2025-05-24 14:46:22 +0200

    update

--- 03_multiple_params.py
+++ 03_multiple_params.py
@@ -2,7 +2,9 @@ import random, time
 
 # define a function
 def coffee_maker(coffee_type, water, beans):
-    if water and beans:
+    coffee_types = ["Espresso", "Doppio", "Latte", "Cappuccino"]
+
+    if water and beans and coffee_type in coffee_types:
         print(f"Making a {coffee_type}...")
         time.sleep(random.randint(1, 4))
         print(f"Your {coffee_type} is ready :)")
@@ -11,6 +13,9 @@ def coffee_maker(coffee_type, water, beans):
             print("Fill up the water tank.")
         if not beans:
             print("Fill up beans.")
+        if not coffee_type in coffee_types:
+            print(f"{coffee_type} is not in my program.")
+            print(f"I can make these types of coffee: {coffee_types}")
 
 
-coffee_maker("Latte", True, True)
+coffee_maker("Cappuccino", True, True)
