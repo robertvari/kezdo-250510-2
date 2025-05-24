26ccb8ba8d21cc46c61b404240db606b1aaebd3a
Robert Vari <mail.robertvari@gmail.com>
2025-05-24 17:29:10 +0200

    update

---
+++ 15_function_libraries.py
@@ -0,0 +1,8 @@
+# import math_functions and use and alias
+from my_functions import math_functions as mf
+
+# explicit import
+from my_functions.math_functions import add_numbers, multiply_numbers
+
+# import all: *
+# from my_functions.math_functions import *
