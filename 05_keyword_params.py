ce2707f48a2f4ef39fddc4ec3449b9ae38e3f1f3
Robert Vari <mail.robertvari@gmail.com>
2025-05-24 14:58:47 +0200

    update

---
+++ 05_keyword_params.py
@@ -0,0 +1,25 @@
+import random, time
+
+# define a function
+def coffee_maker(coffee_type, water, beans):
+    coffee_types = ["Espresso", "Doppio", "Latte", "Cappuccino"]
+
+    if water and beans and coffee_type in coffee_types:
+        print(f"Making a {coffee_type}...")
+        time.sleep(random.randint(1, 4))
+        print(f"Your {coffee_type} is ready :)")
+    else:
+        if not water:
+            print("Fill up the water tank.")
+        if not beans:
+            print("Fill up beans.")
+        if not coffee_type in coffee_types:
+            print(f"{coffee_type} is not in my program.")
+            print(f"I can make these types of coffee: {coffee_types}")
+
+
+coffee_maker(
+    water=True, 
+    coffee_type="Latte", 
+    beans=True
+    )
