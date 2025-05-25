89f6117767927c5fb2011d072dc49874339a10f5
Robert Vari <mail.robertvari@gmail.com>
2025-05-25 18:06:31 +0200

    update

--- TMDB_get_posters.py
+++ TMDB_get_posters.py
@@ -10,7 +10,22 @@ def main():
     download_posters(movie_list)
 
 def get_popular_movies():
-    pass
+    movies = tmdb.Movies()
+    popular_movies = movies.popular(page=1)
+    return popular_movies["results"]
 
 def download_posters(movie_list):
-    pass
+    for movie_data in movie_list:
+        if not movie_data.get("poster_path"): continue
+
+        poster_url = f"{POSTER_ROOT}{movie_data.get('poster_path')}"
+        image_file_path = os.path.join(POSTER_CACHE_FOLDER, movie_data.get('poster_path')[1:])
+        
+        print(f"Downloading: {poster_url}")
+
+        image_data = requests.get(poster_url).content
+        with open(image_file_path, "wb") as file:
+            file.write(image_data)
+
+
+main()
