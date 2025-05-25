844351a190ac2966929b2e0463590ed70be2e5c2
Robert Vari <mail.robertvari@gmail.com>
2025-05-25 15:51:54 +0200

    update

--- my_functions/file_utils.py
+++ my_functions/file_utils.py
@@ -10,14 +10,35 @@ def get_files(root_folder: str, file_list: list, filter: str=None):
         filter (str) (optional): Filter out files with a string. 
     """
 
+    # error checks
     assert os.path.isdir(root_folder), f"{root_folder} must be a folder."
     assert isinstance(file_list, list), "file_list must be a type of list."
 
     if filter:
         assert isinstance(filter, str), "filter must be a type of string."
 
-    
+    # collect files and folders in root_folder
+    folder_content = os.listdir(root_folder)
+
+    # separate folders and files
+    folders = []
+    for i in folder_content:
+        abs_path = os.path.join(root_folder, i)
+        if os.path.isfile(abs_path):
+            # add files to file_list
+            # TODO if we have filter check file
+            file_list.append(abs_path)
+        else:
+            # collect folders
+            folders.append(abs_path)
+
+    # Recursion: if we found folder call get_files() again with the first folder
+    for folder in folders:
+        get_files(folder, file_list, filter)
 
 if __name__ == "__main__":
-    my_photos = "Robert"
-    get_files("fefwfewfw", my_photos, filter=".jpg")
+    my_photos = []
+    # r = raw string
+    get_files(r"C:\Work\PythonSuli", my_photos, filter=".jpg")
+
+    print(f"Found files: {len(my_photos)}")
