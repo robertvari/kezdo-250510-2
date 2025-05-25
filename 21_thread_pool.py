ad0eec4dc5dc0a7b2d44cbe87928fc87d232eee9
Robert Vari <mail.robertvari@gmail.com>
2025-05-25 17:41:35 +0200

    update

--- 21_thread_pool.py
+++ 21_thread_pool.py
@@ -1,4 +1,4 @@
-import time, random
+import time, random, queue, threading
 
 file_list = [
     "image_0001.jpg",
@@ -10,13 +10,30 @@ file_list = [
     "image_0007.jpg",
     "image_0008.jpg",
     "image_0009.jpg",
-    "image_0010.jpg"
+    "image_0010.jpg",
+    "image_0011.jpg",
+    "image_0012.jpg",
+    "image_0013.jpg",
+    "image_0014.jpg",
+    "image_0015.jpg",
 ]
 
-def image_converter(image):
-    print(f"Converting {image}...")
-    time.sleep(random.randint(1,4))
-    print(f"{image} finished.")
-
+# add files to job_queue
+job_queue = queue.Queue()
 for i in file_list:
-    image_converter(i)
+    job_queue.put(i)
+
+
+def image_converter():
+    while not job_queue.empty():
+        image = job_queue.get()
+
+        print(f"Converting {image}...")
+        time.sleep(random.randint(3, 10))
+        print(f"Convert finished: {image}")
+
+        job_queue.task_done()
+
+for _ in range(16):
+    t = threading.Thread(target=image_converter)
+    t.start()
